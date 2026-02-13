f270a8eef476b4db128a355f458442827c4967f0
Wanjirrru <melissawanjiru@yahoo.com>
2026-02-13 12:50:08 +0300

    feat: add gender to cats

--- backend/app/Models/Cat.php
+++ backend/app/Models/Cat.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
-    protected $fillable = ['name', 'description', 'breed', 'age', 'is_adopted', 'images', 'price'];
+    protected $fillable = ['name', 'description', 'breed', 'gender','age', 'is_adopted', 'images', 'price'];
 
     protected $casts = [
         'images' => 'array'
